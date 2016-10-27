4a9d5f2fba92c2a63c2d9f3391828ad2eb223670
brragul <br.ragul@gmail.com>
2016-10-27 17:55:03 -0400

    Modified xmlManipulation.js file

--- js/xmlManipulate.js
+++ js/xmlManipulate.js
@@ -1,20 +1,33 @@
 $("document").ready(function(){
-	$("body").on('click','#getXML',function(){
-		$.getJSON("data/test.json", function(json) {
-			//var obj = json.
-			//console.log(json.Clip_Start_Call.pageName);
+	var myjson,t,tc;
+	$.getJSON("data/video.json", function(json) {
+			myjson = json;
 			$.each(json,function(k,v){
-				var html = "";
+				$(".dropdown-menu").append('<li><a href="#">'+k+'</a></li>');
 			});
 		});
-		
+	$("body").on('click','.dropdown ul li',function(e){
+		 t = e.target.childNodes["0"].data;
+		 tc = myjson[t];
+		$(".dropdown-toggle").html(t);
+	});
+	
+	$("#getJSON").click(function(){
+		$("#myTable").html(displayReq(tc));
 	});
 });
 
 
-function displayReq(){
-	var header = '<tr><th>Variable</th><th>Expected Values</th></tr>';
-	
+function displayReq(o){
+	var header = '<tr><th>Variable</th><th>Expected Values</th></tr>',col;
+	for(var obj in o)
+	{
+		if(o[obj].indexOf(">")>-1||o[obj].indexOf("<")>-1){
+			o[obj] = o[obj].replace('>','&gt;')
+			o[obj] = o[obj].replace('<','&lt;')
+		}
+	col += '<tr><td>'+obj+'</td><td>'+o[obj]+'</td></tr>';
+	}
 	var h = '<table>'+header+col+'</table>';
 	return h;
 }
